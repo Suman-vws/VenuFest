aff0d120cfd3b091bff6b562fa76b241e74ede2a
Suman-vws <suman.chatterjee90@gmail.com>
2016-12-04 00:23:09 +0530

    Initial Commit

---
+++ VenuFest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VenuFest
+//
+//  Created by Sayan Chatterjee on 04/12/16.
+//  Copyright © 2016 Sayan Chatterjee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VenuFest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VenuFest
+//
+//  Created by Sayan Chatterjee on 04/12/16.
+//  Copyright © 2016 Sayan Chatterjee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
