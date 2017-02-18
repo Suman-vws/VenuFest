a234cb2c04cb774cc42af96a7e13d6a9d0cabc1b
Suman-vws <suman.chatterjee90@gmail.com>
2017-02-18 11:52:55 +0530

    network manager changes

---
+++ VenuFest/VFModels/Social/SocialUser.h
@@ -0,0 +1,13 @@
+//
+//  SocialUser.h
+//  VenuFest
+//
+//  Created by Sayan Chatterjee on 18/02/17.
+//  Copyright © 2017 Sayan Chatterjee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SocialUser : NSObject
+
+@end
